9ea82cb848a0544c8e825ee72d368f77ff0c3aec
Rafal Gorczynski <rafal.gorczynski@gmail.com>
2019-05-19 15:01:31 +0200

    by country introduced, postman collection added

--- airports-service/src/main/java/com/rcode/airports/service/AirportController.java
+++ airports-service/src/main/java/com/rcode/airports/service/AirportController.java
@@ -1,7 +1,16 @@
 package com.rcode.airports.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("airports")
@@ -18,5 +27,21 @@ public class AirportController {
     @GetMapping(value = "", produces = "application/json")
     public Iterable<Airport> all() {
         return repository.findAll();
+    }
+
+    @GetMapping(value = "/by/country", produces = "application/json")
+    @Transactional
+    public Map<String, List<Airport>> byCountry() {
+        try (final Stream<Airport> airtportStream = repository.readAllByCountryIsNotNull()) {
+            return airtportStream.collect(Collectors.groupingBy(Airport::getCountry));
+        }
+    }
+
+    @GetMapping(value = "/by/country/{text}", produces = "application/json")
+    @Transactional
+    public Map<String, List<Airport>> byCountry(@PathVariable final String text) {
+        try (final Stream<Airport> airportStream = repository.readAllByCountryContains(text)) {
+            return airportStream.collect(Collectors.groupingBy(Airport::getCountry));
+        }
     }
 }
--- airports-service/src/main/java/com/rcode/airports/service/AirportRepository.java
+++ airports-service/src/main/java/com/rcode/airports/service/AirportRepository.java
@@ -3,6 +3,12 @@ package com.rcode.airports.service;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface AirportRepository extends PagingAndSortingRepository<Airport, Integer> {
+
+    Stream<Airport> readAllByCountryIsNotNull();
+
+    Stream<Airport> readAllByCountryContains(final String text);
 }
