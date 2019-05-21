626098ffb9025a0c916e76428d8a6d11b4ac40c9
Rafal Gorczynski <rafal.gorczynski@gmail.com>
2019-05-21 18:49:58 +0200

    work in progress

---
+++ airports-service/src/test/java/com/rcode/airports/service/AirportControllerTest.java
@@ -0,0 +1,17 @@
+package com.rcode.airports.service;
+
+import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class AirportControllerTest {
+
+    @MockBean
+    private AirportRepository airportRepository;
+
+
+    @Test
+    public void testGetAll() {
+        MockMvc.
+    }
+}
